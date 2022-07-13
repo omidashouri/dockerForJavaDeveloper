0c9beb2c09dfcfbb654aea2e1a17ad5c583b4486
omidashouri <omidashouri@gmail.com>
2022-07-14 00:06:15 +0430

    Initial commit

---
+++ src/test/java/com/example/dockerforjavadeveloper/DockerForJavaDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerforjavadeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerForJavaDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
